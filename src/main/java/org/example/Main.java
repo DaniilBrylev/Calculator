9cc27eba1e09e473af560730edbd7fd492e471b5
DaniilBrylev <dan.brylev38@gmail.ru>
2024-09-29 19:50:55 +0300

    2r3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
         public class Main {
             public static void main(String[] args) {
                 Scanner scanner = new Scanner(System.in);
-                System.out.print("Введите первое число:");
+                System.out.print("Введwefrghyjukiите первое число:");
                 int num1 = scanner.nextInt();
                 if (num1 == 0) {
                     System.out.println("Калькулятор выключен");
