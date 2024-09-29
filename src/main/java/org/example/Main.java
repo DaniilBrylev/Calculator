b996e0bbd62e1bd5eea743af3ebc4e83c58bb47c
DaniilBrylev <dan.brylev38@gmail.ru>
2024-09-29 20:27:04 +0300

    Починил машину

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
         public class Main {
             public static void main(String[] args) {
                 Scanner scanner = new Scanner(System.in);
-                System.out.print("Введwefrghyjukiите первое число:");
+                System.out.print("Введите первое число починил:");
                 int num1 = scanner.nextInt();
                 if (num1 == 0) {
                     System.out.println("Калькулятор выключен");
                 }
                 else {
-                    System.out.print("Введите вetddwfторое число:");
+                    System.out.print("Введите второе число:");
                     int num2 = scanner.nextInt();
 
                     int res;
